ea4abc5322eccda50d6a038eaba6286feb24e03a
Joseph <z@charmed.tech>
2019-07-04 10:33:13 -0700

    Use eslint, not prettier

--- controllers/blocklist.server.js
+++
@@ -1,6 +0,0 @@
-const local_blocklist = require('./local.blocklist');
-const phishing_army = require('./phishing-army.blocklist');
-
-const blocklist = [].concat(local_blocklist, phishing_army);
-
-module.exports = blocklist;
